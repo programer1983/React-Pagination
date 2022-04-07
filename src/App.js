f82d19edc97b288907712ce646de56a2215cee15
programer1983 <programer972@gmail.com>
2022-04-07 17:39:24 +0300

    commit-6;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react"
 import axios from "axios"
 import './App.css';
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -19,11 +20,15 @@ function App() {
     fetchPosts()
   }, [])
 
-   console.log(posts)
+   const indexOfLastPost = currentpage * postsPerPage
+   const indexOfFirstPost = indexOfLastPost - setPostsPerPage
+   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
     </div>
   );
 }
