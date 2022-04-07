498fd649c116f9345d9abf3fd99ee6ee4936999e
programer1983 <programer972@gmail.com>
2022-04-07 16:36:21 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import {useState, useEffect} from "react"
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentpage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+
+
   return (
     <div className="App">
       <h1>Hello Dimon</h1>
