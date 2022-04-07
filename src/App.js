99999616392d9b7ad8f0d549bab7535f2fa2567e
programer1983 <programer972@gmail.com>
2022-04-07 16:43:05 +0300

    commit-4;

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
   const [currentpage, setCurrentPage] = useState(1)
   const [postsPerPage, setPostsPerPage] = useState(10)
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      setPosts(res.data)
+      setLoading(false)
+    }
+    fetchPosts()
+  }, [])
+
 
   return (
     <div className="App">
